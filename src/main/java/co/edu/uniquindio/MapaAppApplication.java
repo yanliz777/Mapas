b299a49e58bf39c7a478c934af41787c666c8bc2
repoYan <yan_cristiano@hotmail.com>
2024-06-25 12:11:00 -0500

    Creación del proyecto mapas

---
+++ src/main/java/co/edu/uniquindio/MapaAppApplication.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaAppApplication.class, args);
+		System.out.println("hola mundo");
+	}
+
+}
---
+++ src/test/java/co/edu/uniquindio/MapaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
