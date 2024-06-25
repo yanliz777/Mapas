061d46142fc4037aff94c45f8971ab14735e1fc7
repoYan <yan_cristiano@hotmail.com>
2024-06-25 12:45:42 -0500

    Se creo la [200~clase principal del proyecto Spring. Acá estamos configurando nuestro proyecto

---
+++ src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+Esta clase será la clase principal del proyecto Spring. Acá estamos configurando nuestro proyecto
+para que haga uso de Spring Boot. Cuando queramos desplegar el proyecto, simplemente
+ejecutamos el main de esta clase.
+ */
+
+@SpringBootApplication
+public class ProyectoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoApplication.class, args);
+        System.out.println("Hola mundo");
+    }
+}
